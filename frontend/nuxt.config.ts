bd8d3a71cccb7780ce96c8c084d1f1d9aada4baa
Hung Pham <phamphihungbk@gmail.com>
2026-04-12 02:37:39 +0200

    fix: nginx for backend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
